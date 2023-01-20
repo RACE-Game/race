8da9c313a9129152faad1afdf491a95e0240257a
team-race <race.game.team@gmail.com>
2023-01-21 01:08:28 +0800

    Impl fn to find and return straight(s) in cards
    The worst-case performance is O(3n) where the max of n 11

--- examples/holdem/src/evaluator.rs
+++ examples/holdem/src/evaluator.rs
@@ -1,6 +1,7 @@
 // use serde::{Deserialize, Serialize};
 // use race_core::error::{Error, Result};
 use std::cmp::Ordering;
+use std::collections::HashMap;
 
 pub fn create_cards<'a>(
     community_cards: &[&'a str; 5],
@@ -45,7 +46,65 @@ pub fn get_sorted_kinds<'a>(cards: &Vec<&'a str>) -> Vec<&'a str> {
     sorted_kinds
 }
 
+// Consider merging this fn into the above
+pub fn get_suits<'a>(cards: &Vec<&'a str>) -> Vec<&'a str> {
+    let mut suits: Vec<&str> = Vec::new();
+    for card in cards {
+        let (suit, _) = card.split_at(1);
+        suits.push(suit);
+    }
+
+    suits
+}
+
+pub fn get_flush_cards<'a>(cards: &Vec<&'a str>) -> Vec<&'a str> {
+    let mut groups: HashMap<&'a str, Vec<&'a str>> = HashMap::new();
+
+    for card in cards {
+        let (suit, _) = card.split_at(1);
+        groups.entry(suit)
+            .and_modify(|grp| grp.push(card))
+            .or_insert(vec![card]);
+    }
+
+    for (_, val) in groups.iter() {
+        if val.len() >= 5 {
+            return val.clone();
+        }
+    }
+
+    vec![]
+}
+
+pub fn flush_cards<'a>(cards: &Vec<&'a str>) -> Vec<&'a str> {
+    let mut suit_freq: Vec<(&str, u8)> = vec![
+        ("c", 0),
+        ("d", 0),
+        ("h", 0),
+        ("s", 0)
+    ];
+
+    let suits = get_suits(cards);
+
+    for suit in suits {
+        if suit == "c" {suit_freq[0].1 += 1;}
+        else if suit == "d" {suit_freq[1].1 += 1;}
+        else if suit == "h" {suit_freq[2].1 += 1;}
+        else {suit_freq[3].1 += 1;}
+    }
+
+    for freq in suit_freq {
+        if freq.1 >= 5 {
+            // Only if cards: Vec<&'a str> can be iterated but Iterator not impl-ed for &str
+        }
+    }
+
+    vec![]
+}
+
 pub fn evaluate_cards(cards: Vec<&str>, kinds: Vec<&str>) -> u8 {
+    let flush_cards = get_flush_cards(&cards);
+
     // royal flush
 
     // straight flush
@@ -57,7 +116,9 @@ pub fn evaluate_cards(cards: Vec<&str>, kinds: Vec<&str>) -> u8 {
     // full house
 
     // flush
-
+    else if flush_cards.len() == 5 {
+        5
+    }
     // straight
 
     // three of a kind
@@ -91,10 +152,11 @@ mod tests {
         let hand1: [&str; 2] = ["ca", "c4"]; // pair A
         let hand2: [&str; 2] = ["d4", "h9"]; // High Card A
 
+
         let mut cards = create_cards(&community_cards, &hand1);
         // cards.extend_from_slice(&community_cards);
         // cards.extend_from_slice(&hand1);
-        cards.sort_by(|c1,c2| compare_kinds(c1, c2));
+        cards.sort_by(|c1, c2| compare_kinds(c1, c2));
 
         let sorted_kinds = get_sorted_kinds(&cards);
 
@@ -109,6 +171,14 @@ mod tests {
 
         let result: u8 = evaluate_cards(cards, sorted_kinds);
         assert_eq!(1, result); // passed
+
+        // Test flush
+        let cmt_cards: [&str; 5] = ["da", "d2", "c7", "d6", "d5"];
+        let mut new_cards = create_cards(&cmt_cards, &hand2);
+        cards.sort_by(|c1, c2| compare_kinds(c1, c2));
+
+        let flush_cards = get_flush_cards(&new_cards);
+        assert_eq!(5, flush_cards.len());
 
 
     }
