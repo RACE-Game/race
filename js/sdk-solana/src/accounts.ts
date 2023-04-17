62ae34ebcb38a0fa33f9154e85e7b96edca16747
DogLooksGood <doglooksgood@hotmail.com>
2023-04-18 04:07:36 +0800

    Add PlayerState

--- js/sdk-solana/src/accounts.ts
+++ js/sdk-solana/src/accounts.ts
@@ -1,25 +1,49 @@
-import { field, option, vec } from "@dao-xyz/borsh";
 import { PublicKey } from "@solana/web3.js";
+import * as borsh from 'borsh';
 
+export class ExtendedWriter extends borsh.BinaryWriter {
+  writePublicKey(value: PublicKey) {
+    let buffer = value.toBuffer();
+    this.writeFixedArray(buffer)
+  }
 
-
-export class PlayerState {
-  @field({ type: 'bool' })
-  is_initialized: boolean;
-
-  @field({ type: 'string' })
-  nick: string
-
-  @field({ type: option(PublicKey) })
-  pfp?: PublicKey
-
-  @field({ type: vec('u8') })
-  padding: Uint8Array
-
-  constructor(data: PlayerState) {
-    this.is_initialized = data.is_initialized;
-    this.nick = data.nick;
-    this.pfp = data.pfp;
-    this.padding = data.padding
+  writeBool(value: boolean) {
+    this.writeU8(value === true ? 1 : 0)
   }
 }
+
+export class ExtendedReader extends borsh.BinaryReader {
+  readPublicKey() {
+    const value = this.readFixedArray(32);
+    return new PublicKey(value)
+  }
+
+  readBool() {
+    const value = this.readU8();
+    return value === 1
+  }
+}
+
+export class PlayerState {
+  isInitialized: boolean;
+  nick: string
+  pfp?: PublicKey
+
+  constructor(data: PlayerState) {
+    this.isInitialized = data.isInitialized;
+    this.nick = data.nick;
+    this.pfp = data.pfp;
+  }
+}
+
+export const playerStateSchema =
+  new Map([[PlayerState, {
+    kind: 'struct',
+    fields: [
+      ['isInitialized', 'bool'],
+      ['nick', 'string'],
+      ['pfp', {
+        kind: 'option',
+        type: 'publicKey',
+      }]]
+  }]]);
--- js/sdk-solana/tests/accounts.spec.ts
+++ js/sdk-solana/tests/accounts.spec.ts
@@ -1,21 +1,22 @@
 import { assert } from 'chai';
-import { PlayerState } from '../src/accounts';
 import {
-  deserialize,
-  serialize,
-} from "@dao-xyz/borsh";
+  PlayerState,
+  ExtendedReader,
+  ExtendedWriter,
+  playerStateSchema
+} from '../src/accounts';
+import * as borsh from 'borsh';
 import { PublicKey } from '@solana/web3.js';
 
 describe('Test account data serialization', () => {
   it('PlayerState', () => {
     let state = new PlayerState({
-      is_initialized: true,
+      isInitialized: true,
       nick: 'Alice',
       pfp: PublicKey.default,
-      padding: Uint8Array.from([0, 0, 0])
     });
-    let serialized = serialize(state);
-    let deserialized = deserialize(serialized, PlayerState);
+    let buf = borsh.serialize(playerStateSchema, state, ExtendedWriter)
+    let deserialized = borsh.deserializeUnchecked(playerStateSchema, PlayerState, Buffer.from(buf), ExtendedReader);
     assert.deepStrictEqual(state, deserialized);
   })
 })
