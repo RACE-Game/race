97b50f5fd90d835605cc80ed14c7aa737acbcd8c
team-race <race.game.team@gmail.com>
2023-04-26 17:29:59 +0800

    Update addrs in constants

--- examples/demo-app/src/constants.ts
+++ examples/demo-app/src/constants.ts
@@ -2,7 +2,7 @@ import { Chain } from './types';
 
 export const CHAIN_TO_REG_ADDR: Record<Chain, string> = {
   'facade': 'DEFAULT_REGISTRATION_ADDRESS',
-  'solana': 'DUMZU76rYgYkXuaCBB2m65USqeeCd1T1Gk2mpfgKKrS4',
+  'solana': 'Ha6yNYPpUxGNJZhaxht4Ma3wFkquwNyjVoEaJ3V5Cj1k',
 };
 
 export const CHAIN_TO_RPC: Record<Chain, string> = {
@@ -12,7 +12,7 @@ export const CHAIN_TO_RPC: Record<Chain, string> = {
 
 export const CHAIN_ADDR_GAME_MAPPING: Record<Chain, Record<string, string>> = {
   'solana': {
-    '96VJaPnUHuCGsvX1eACQPDU5R7WxwB2TxQY6CL947DqF': 'raffle'
+    'TpSf11nmAb9GX5hr3UnKiTrU67sKV77Ttt7AaCSfwWr': 'raffle'
   },
   'facade': {
     'CHAT_BUNDLE_ADDRESS': 'chat',
