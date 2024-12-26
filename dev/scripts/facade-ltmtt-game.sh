6604a671a61b7372a2a719019da10ce0e31331e4
LX <lxedge@sina.com>
2024-12-26 23:35:16 +0800

    chore: change game account data

--- dev/scripts/facade-ltmtt-game.sh
+++ dev/scripts/facade-ltmtt-game.sh
@@ -3,7 +3,7 @@ START_TIME=$(expr $TIME + 60000)
 CLOSE_TIME=$(expr $TIME + 120000)
 SETTLE_TIME=$(expr $TIME + 1800000)
 TICKET=100000000
-TABLE_SIZE=3
+TABLE_SIZE=9
 START_CHIPS=100000
 
 echo "Delete test db files"
@@ -20,13 +20,14 @@ DATA=$(cd ./js/borsh; npx ts-node ./bin/cli.ts \
 			  -u64 "$CLOSE_TIME" \
 			  -u64 "$SETTLE_TIME" \
 			  -u8 "$TABLE_SIZE" \
-			  -u64 "$TICKET" \
-			  -u64 "$START_CHIPS")
+			  -u32 0 \
+			  -u64 10000 \
+			  -s "raceholdemtargetraceholdemmtttablewasm")
 echo "DATA is $DATA"
 
 JSON=$(cat <<EOF
 {
-  "title": "MTT",
+  "title": "LTMTT",
   "bundle": "../race-holdem/target/race_holdem_ltmtt.wasm",
   "token": "FACADE_USDC",
   "maxPlayers": 10,
