b4a17829fc5b9d5b8110bc3aba718e6648995be8
DogLooksGood <doglooksgood@gmail.com>
2025-02-06 14:22:43 +0800

    Bugfix

--- js/sdk-sui/src/constants.ts
+++ js/sdk-sui/src/constants.ts
@@ -1,7 +1,7 @@
 export const PACKAGE_ID = "0x8dfab7b70810c74c61e15b0ea98c38357b13453a2d30fc1169e2f791a1560aa6"
-export const PROFILE_STRUCT_TYPE = `${PACKAGE_ID}::profile::PlayerProfile`
-export const SERVER_STRUCT_TYPE = `${PACKAGE_ID}::server::Server`
-export const GAME_STRUCT_TYPE = `${PACKAGE_ID}::game::Game`
+export const PROFILE_MODULE_STRUCT = `profile::PlayerProfile`
+export const SERVER_MODULE_STRUCT = `server::Server`
+export const GAME_MODULE_STRUCT = `game::Game`
 export const GAS_BUDGET = 50_000_000n
 export const MAXIMUM_TITLE_LENGTH = 16;
 export const SUI_ICON_URL = 'https://assets.coingecko.com/coins/images/26375/standard/sui-ocean-square.png'
